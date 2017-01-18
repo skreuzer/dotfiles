5ecc466b21906858f189cba867dc7941c7e17992
Steven Kreuzer <steven@FreeBSD.org>
2017-01-18 10:42:26 -0500

    Add a function to display the expiration date of a SSL certificate

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -85,3 +85,6 @@ function cflastrun {
         /var/cfengine/promise_summary.log
 }
 
+function sslexpire {
+	openssl s_client -connect $1:443 2> /dev/null < /dev/null | openssl x509 -noout -dates
+}
