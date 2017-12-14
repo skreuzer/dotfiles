2a0eecb2734805ce2eb81bff94b2c5c992fb26e6
Steven Kreuzer <skreuzer@FreeBSD.org>
2017-12-14 09:53:37 -0500

    Update grep alias
    Unbreak colors in grep v2.27 and exclude .svn and .git directories

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,4 @@
-alias grep='GREP_COLOR="1;37;41" LANG=C grep --color=auto'
+alias grep='GREP_COLOR="1;31" LANG=C grep --exclude-dir=.svn --exclude-dir=.git --color=auto'
 alias ipsort="sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4"
 alias sl='ls'
 alias pi='echo "scale=39;4*a(1)" | bc -l'
